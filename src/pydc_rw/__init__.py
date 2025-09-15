795be5b001e10f09ae9ed24143139ef5e0e35c1c
mbhonsle <6602374+mbhonsle@users.noreply.github.com>
2025-09-15 03:47:58 -0700

    rename connectors -> writers

--- src/pydc_rw/__init__.py
+++ src/pydc_rw/__init__.py
@@ -1,5 +1,5 @@
 from .readers.query_svc import QueryServiceClient
-from .connectors.ingestion_api.ingestion_client import DataCloudIngestionClient
+from .writers.ingestion_api.ingestion_client import DataCloudIngestionClient
 
 __all__ = [
     "QueryServiceClient",
